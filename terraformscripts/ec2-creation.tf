3128bac987a054cd989f4ba743af063e6a91284e
Pradeep <160380277+pkumard4@users.noreply.github.com>
2024-04-23 11:13:26 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,17 +1,15 @@
-provider "aws"{
-region = "ap-south-1"
-}
-
-
-resource "aws_instance"  "AWSServer"{
-ami = "ami-0e670eb768a5fc3d4"
-instance_type = "t2.micro"
-key_name = "devopsdecbatchmss"
-security_groups = ["launch-wizard-2"]
-tags = {
-Name = "TerraformServer"
-}
-}
+provider "aws"{ 
+                    region="ap-south-1"
+                }
+resource "aws_instance" "AWSEC2"{ 
+                                        ami = "ami-001843b876406202a"
+                                        instance_type = "t2.micro" 
+                                        key_name = "pradeepthi.pem"
+                                        security_group = ["launch-wizard-2"] 
+                                        tags = {
+                                                    Name = "terraformserver" 
+                                                }
+                                        }
 
 
 
