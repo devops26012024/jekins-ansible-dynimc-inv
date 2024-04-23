81ea1305f37172a589a48f043192b9540501cb5f
Pradeep <160380277+pkumard4@users.noreply.github.com>
2024-04-23 13:55:12 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -4,7 +4,7 @@ provider "aws"{
 resource "aws_instance" "AWSEC2"{ 
                                         ami = "ami-001843b876406202a"
                                         instance_type = "t2.micro" 
-                                        key_name = "pradeepthi.pem"
+                                        key_name = "pradeepthi"
                                         security_groups = ["launch-wizard-2"] 
                                         tags = {
                                                     Name = "terraformserver" 
